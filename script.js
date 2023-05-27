7593a22942a3dd8b89087ff222bf16a6de02d9a5
unknown <laghmambilal49@gmail.com>
2023-05-27 11:39:18 +0100

    add show result of player

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var cell = document.getElementsByClassName("cell");
 var playerOne = [];
 var playerTwo = [];
 var turn = 1;
-var countOne = 0
-var count = 0
+var count = 0;
+var result_First = 0;
+var result_Second = 0;
+var res = false;
+function reset() {
+    for (var i = 0; i < cell.length; i++) {
+        cell[i].innerHTML = ""
+    }
+    res = true
+}
 for (var i = 0; i < cell.length; i++) {
     cell[i].onclick = function () {
         if (this.innerHTML == "") {
@@ -33,7 +41,7 @@ for (var i = 0; i < cell.length; i++) {
         for (var i = 0; i < playerTwo.length; i++) {
             choicTwo.push(playerTwo[i])
         }
-        var rslt = "";
+        var winner = "";
         for (var i = 0; i < choicOne.length; i++) {
             for (var j = 1; j < choicOne.length; j++) {
                 for (var k = 2; k < choicOne.length; k++) {
@@ -41,73 +49,30 @@ for (var i = 0; i < cell.length; i++) {
                         (choicOne[i] == 7 && choicOne[j] == 8 && choicOne[k] == 9) || (choicOne[i] == 1 && choicOne[j] == 5 && choicOne[k] == 9) ||
                         (choicOne[i] == 3 && choicOne[j] == 5 && choicOne[k] == 7) || (choicOne[i] == 1 && choicOne[j] == 4 && choicOne[k] == 7) ||
                         (choicOne[i] == 2 && choicOne[j] == 5 && choicOne[k] == 8) || (choicOne[i] == 3 && choicOne[j] == 6 && choicOne[k] == 9)) {
-                        rslt = "one"
-                        break
+                        winner = "firstPlayer"
                     } else if ((choicTwo[i] == 1 && choicTwo[j] == 2 && choicTwo[k] == 3) || (choicTwo[i] == 4 && choicTwo[j] == 5 && choicTwo[k] == 6) ||
                         (choicTwo[i] == 7 && choicTwo[j] == 8 && choicTwo[k] == 9) || (choicTwo[i] == 1 && choicTwo[j] == 5 && choicTwo[k] == 9) ||
                         (choicTwo[i] == 3 && choicTwo[j] == 5 && choicTwo[k] == 7)) {
-                        rslt = "two"
-                        break
+                        winner = "secondPlayer"
                     }
                 }
             }
         }
-        if (rslt === "one" && count >= 3) {
+
+        if (winner === "firstPlayer" && count >= 3 && !res) {
             alert("X are Won!");
-            // for(var x = 0 ; x < cell.length ; x++){
-            //     cell[x].innerHTML = "?"
-            // }
-        } else if (rslt === "two"  && count >= 3) {
-            alert("O are Won!")
-        }else if((count == 5) && (rslt != "one" && rslt != "two")){
+            result_First += 1;
+            document.getElementById("showResult").innerHTML = `X: ${result_First} <br> O is: ${result_Second}`
+
+        } else if (winner === "secondPlayer" && count >= 3 && !res) {
+            alert("O are Won!");
+            result_Second += 1;
+            document.getElementById("showResult").innerHTML = `X: ${result_First} <br> O is: ${result_Second}`
+
+        } else if ((count == 5) && (winner != "firstPlayer" && winner != "secondPlayer" && !res)) {
             alert("Nobody Won!")
+            document.getElementById("showResult").innerHTML = `X: ${result_First} <br> O is: ${result_Second}`
         }
     }
-
+    document.getElementById("showResult").innerHTML = `X: ${result_First} <br> O is: ${result_Second}`
 }
-
-
-
-// '1', '5', '9', '7', '2'
-// document.getElementById("check").onclick = function () {
-//     playerOne.sort(function(a,b){
-//         return a-b
-//     })
-//     var choicOne = []
-//     for(var i=0;i < playerOne.length;i++){
-//         choicOne.push(playerOne[i])
-//     }
-//     playerTwo.sort(function(a,b){
-//         return a-b
-//     })
-//     var choicTwo = []
-//     for(var i=0;i < playerTwo.length;i++){
-//         choicTwo.push(playerTwo[i])
-//     }
-//     var rslt = "";
-//     for (var i = 0; i < choicOne.length; i++) {
-//         for (var j = 1; j < choicOne.length; j++) {
-//             for (var k = 2; k < choicOne.length; k++) {
-//                 if ((choicOne[i] == 1 && choicOne[j] == 2 && choicOne[k] == 3) || (choicOne[i] == 4 && choicOne[j] == 5 && choicOne[k] == 6) ||
-//                     (choicOne[i] == 7 && choicOne[j] == 8 && choicOne[k] == 9) || (choicOne[i] == 1 && choicOne[j] == 5 && choicOne[k] == 9) ||
-//                     (choicOne[i] == 3 && choicOne[j] == 5 && choicOne[k] == 7) || (choicOne[i] == 1 && choicOne[j] == 4 && choicOne[k] == 7) ||
-//                     (choicOne[i] == 2 && choicOne[j] == 5 && choicOne[k] == 8)|| (choicOne[i] == 3 && choicOne[j] == 6 && choicOne[k] == 9)) {
-//                     rslt = "one"
-//                 } else if ((choicTwo[i] == 1 && choicTwo[j] == 2 && choicTwo[k] == 3) || (choicTwo[i] == 4 && choicTwo[j] == 5 && choicTwo[k] == 6) ||
-//                     (choicTwo[i] == 7 && choicTwo[j] == 8 && choicTwo[k] == 9) || (choicTwo[i] == 1 && choicTwo[j] == 5 && choicTwo[k] == 9) ||
-//                     (choicTwo[i] == 3 && choicTwo[j] == 5 && choicTwo[k] == 7)) {
-//                     rslt = "two"
-//                 }
-//             }
-//         }
-//     }
-//     if(rslt === "one"){
-//         alert("X is Win!")
-//     }else if(rslt === "two"){
-//         alert("O is Win!")
-//     }else{
-//         alert("nobody Win!")
-//     }
-//     console.log(choicOne.reverse())
-//     console.log(choicTwo.reverse())
-// }
